d6c95529fbbe9cf216f0fed3342e53f696f8e492
dreamlx <dreamlinx@gmail.com>
2025-09-05 10:50:37 +0800

    Fix JavaScript controllers/index.js - remove non-existent @hotwired/stimulus-loading dependency and fix import path

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,4 +1,8 @@
 // Import and register all your controllers from the importmap via controllers/**/*_controller
-import { application } from "controllers/application"
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-eagerLoadControllersFrom("controllers", application)
+import { application } from "./application"
+
+// Configure Stimulus development experience
+application.debug = false
+window.Stimulus = application
+
+export { application }
